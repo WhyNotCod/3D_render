8a5a2970f5a6df2d2e615d5712f4f94dd4fc2d87
RenoVISION <poul2002.02@gmail.com>
2025-02-27 12:59:25 -0500

    hi

--- lib/main.dart
+++ lib/main.dart
@@ -55,16 +55,16 @@ class _RenderState extends State<Render> with SingleTickerProviderStateMixin {
   @override
   void initState() {
     super.initState();
-    _controller = AnimationController(
-        duration: const Duration(microseconds: 30000), vsync: this)
-      ..addListener(() {
-        if (_cube != null) {
-          _cube!.rotation.y = _controller.value * 360;
-          _cube!.updateTransform();
-          _scene.update();
-        }
-      })
-      ..repeat();
+    _controller =
+        AnimationController(duration: const Duration(seconds: 3), vsync: this)
+          ..addListener(() {
+            if (_cube != null) {
+              _cube!.rotation.y = _controller.value * 360;
+              _cube!.updateTransform();
+              _scene.update();
+            }
+          })
+          ..repeat();
   }
 
   @override
